4b8f1f07faf4d611501a01d10c3d8fbca0c50bc2
Gergő Takács <spiroka01@gmail.com>
2020-05-02 15:30:06 +0200

    It works! 🍾

--- lambda/escape-room.js
+++ lambda/escape-room.js
@@ -1,10 +1,12 @@
-import { App, ExpressReceiver } from "@slack/bolt";
+import { App, ExpressReceiver, LogLevel } from "@slack/bolt";
+import serverless from "serverless-http";
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: "/.netlify/functions/escape-room/slack/events",
 });
 
-export const handler = expressReceiver.app;
+export const handler = serverless(expressReceiver.app);
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
