9615e529dd8ba54e32ba57e01d4431bfa390d4a9
Gergő Takács <spiroka01@gmail.com>
2020-05-02 12:44:27 +0200

    Added command listener

---
+++ lambda/escape-room.js
@@ -0,0 +1,18 @@
+import { App, ExpressReceiver } from "@slack/bolt";
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+export const expressApp = expressReceiver.app;
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver,
+});
+
+app.command("/escape-room", async ({ ack, say }) => {
+  await ack();
+
+  await say({ text: "It works!" });
+});
